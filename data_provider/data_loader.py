Update run.py, metrics.py, and data_loader.py with code changes for classification and fixes. Changed model to classificatio, softmax

data_provider/data_loader.py
@@ -671,7 +671,7 @@ class Dataset_Crop(Dataset):
         # init
         assert flag in ["train", "test", "val"]
         type_map = {"train": 0, "val": 1, "test": 2}
-        self.set_type = type_map[flag]
+        self.split_id = type_map[flag]
 
         self.features = features
         self.target = target
@@ -691,12 +691,20 @@ class Dataset_Crop(Dataset):
         df_raw.columns: ['date', ...(other features), target feature]
         """
         cols = list(df_raw.columns)
-        cols.remove(self.target)
+        # target is columns that start with class_name_late (one hot encoding)
+        target = df_raw.columns.str.startswith("class_name_late")
+        print("TARGET", target)
+        print(list(df_raw.columns[target]))
+        self.target = list(df_raw.columns[target])
+        # cols.remove(self.target)
+        cols = [x for x in cols if x not in self.target]
         cols.remove("date")
         cols.remove("FOI_ID_LEVERANCIER")
         groups = df_raw.groupby("FOI_ID_LEVERANCIER")
         print("these are the groups\n", groups)
-        df_raw = df_raw[["date"] + ["FOI_ID_LEVERANCIER"] + cols + [self.target]]
+        df_raw = df_raw[
+            ["date"] + ["FOI_ID_LEVERANCIER"] + cols + self.target
+        ]
 
         num_train = int(len(groups) * 0.7)
         num_test = int(len(groups) * 0.2)
@@ -709,9 +717,9 @@ class Dataset_Crop(Dataset):
             len(groups) - num_test - self.seq_len,
         ]
         border2s = [num_train, num_train + num_vali, len(groups)]
-        # Set_type for train, val, and test
-        border1 = border1s[self.set_type]
-        border2 = border2s[self.set_type]
+        # Border split for train, val, and test
+        border1 = border1s[self.split_id]
+        border2 = border2s[self.split_id]
 
         # Create a list of group keys
         group_keys = groups.groups.keys()
@@ -725,27 +733,30 @@ class Dataset_Crop(Dataset):
         train_groups = groups.filter(lambda x: x.name in train_keys)
         test_groups = groups.filter(lambda x: x.name in test_keys)
         vali_groups = groups.filter(lambda x: x.name in vali_keys)
+        list_of_groups = [train_groups, test_groups, vali_groups]
 
-        cols_data = df_raw.columns[1:]
-        df_data = df_raw[cols_data]
+        amount_features = len(cols)
+        cols_data = df_raw.columns[1 : amount_features + 1]
+        cur_group = list_of_groups[self.split_id]
+        df_data = cur_group[cols_data]
+
         if self.scale:
             self.scaler.fit(df_data.values)
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
 
-        # print("training", train_groups.head())
-        for group_df in [train_groups, vali_groups, test_groups]:
-            group_df = group_df[["date"] + cols + [self.target]]
-            print("group df", group_df.head())
-            if self.features == "M" or self.features == "MS":
-                cols_data = group_df.columns[1:]
-                df_data = group_df[cols_data]
-            elif self.features == "S":
-                df_data = group_df[[self.target]]
+        # # print("training", train_groups.head())
+        # for group_df in [train_groups, vali_groups, test_groups]:
+        #     group_df = group_df[["date"] + cols + [self.target]]
+        #     print("group df", group_df.head())
+        #     if self.features == "M" or self.features == "MS":
+        #         cols_data = group_df.columns[1:]
+        #         df_data = group_df[cols_data]
+        #     elif self.features == "S":
+        #         df_data = group_df[[self.target]]
 
-
-        df_stamp = df_raw[["date"]][border1:border2]
+        df_stamp = cur_group[["date"]]
         df_stamp["date"] = pd.to_datetime(df_stamp.date)
         if self.timeenc == 0:
             df_stamp["month"] = df_stamp.date.apply(
@@ -765,9 +776,10 @@ class Dataset_Crop(Dataset):
             )
             data_stamp = data_stamp.transpose(1, 0)
 
-        self.data_x = list(groups)[border1:border2]
-        self.data_y = list(groups)[border1:border2]
+        self.data_x = cur_group[cols_data].values.astype(np.float64)
+        self.data_y = cur_group[self.target].values.astype(np.float64)
         self.data_stamp = data_stamp
+        print("These are the target values", self.target, cur_group[self.target])
 
     def __getitem__(self, index):
         s_begin = index
@@ -778,12 +790,14 @@ class Dataset_Crop(Dataset):
         seq_x = self.data_x[s_begin:s_end]
         seq_y = self.data_y[r_begin:r_end]
         seq_x_mark = self.data_stamp[s_begin:s_end]
-        seq_y_mark = self.data_stamp[r_begin:r_end]
-
+        seq_y_mark = self.data_stamp[index]
+        seq_y = self.data_y[index]
+        # print(seq_y, self.data_y, self.data_y.shape, self.data_x.shape)
+        # exit()
         return seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
+        return len(self.data_x) - self.seq_len
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)

experiments/exp_long_term_forecasting.py
@@ -57,41 +57,44 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                     batch_y_mark = batch_y_mark.float().to(self.device)
 
                 # decoder input
-                dec_inp = torch.zeros_like(
-                    batch_y[:, -self.args.pred_len :, :]
-                ).float()
-                dec_inp = (
-                    torch.cat(
-                        [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
-                    )
-                    .float()
-                    .to(self.device)
-                )
+                # Decoder turned off for classification, dec_inp=batch_y
+
+                # dec_inp = torch.zeros_like(
+                #     batch_y[:, -self.args.pred_len :, :]
+                # ).float()
+                # dec_inp = (
+                #     torch.cat(
+                #         [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
+                #     )
+                #     .float()
+                #     .to(self.device)
+                # )
                 # encoder - decoder
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
                         if self.args.output_attention:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )[0]
                         else:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )
                 else:
                     if self.args.output_attention:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )[0]
                     else:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )
                 f_dim = -1 if self.args.features == "MS" else 0
-                outputs = outputs[:, -self.args.pred_len :, f_dim:]
-                batch_y = batch_y[:, -self.args.pred_len :, f_dim:].to(
-                    self.device
-                )
+                # outputs = outputs[:, -self.args.pred_len :, f_dim:]
+                # batch_y = batch_y[:, -self.args.pred_len :, f_dim:].to(
+                #     self.device
+                # )
+                outputs = outputs.float()
 
                 pred = outputs.detach().cpu()
                 true = batch_y.detach().cpu()
@@ -147,27 +150,27 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                     batch_y_mark = batch_y_mark.float().to(self.device)
 
                 # decoder input
-                dec_inp = torch.zeros_like(
-                    batch_y[:, -self.args.pred_len :, :]
-                ).float()
-                dec_inp = (
-                    torch.cat(
-                        [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
-                    )
-                    .float()
-                    .to(self.device)
-                )
+                # batch_y = torch.zeros_like(
+                #     batch_y[:, -self.args.pred_len :, :]
+                # ).float()
+                # batch_y = (
+                #     torch.cat(
+                #         [batch_y[:, : self.args.label_len, :], batch_y], dim=1
+                #     )
+                #     .float()
+                #     .to(self.device)
+                # )
 
                 # encoder - decoder
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
                         if self.args.output_attention:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )[0]
                         else:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )
 
                         f_dim = -1 if self.args.features == "MS" else 0
@@ -180,18 +183,18 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                 else:
                     if self.args.output_attention:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )[0]
                     else:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )
 
                     f_dim = -1 if self.args.features == "MS" else 0
-                    outputs = outputs[:, -self.args.pred_len :, f_dim:]
-                    batch_y = batch_y[:, -self.args.pred_len :, f_dim:].to(
-                        self.device
-                    )
+                    # print(outputs.shape, batch_y.shape)
+                    # outputs = outputs[:, -self.args.num_classes :, f_dim:]
+                    # batch_y = batch_y.float().to(self.device)
+                    outputs = outputs.float()
                     loss = criterion(outputs, batch_y)
                     train_loss.append(loss.item())
 
@@ -281,43 +284,44 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                     batch_y_mark = batch_y_mark.float().to(self.device)
 
                 # decoder input
-                dec_inp = torch.zeros_like(
-                    batch_y[:, -self.args.pred_len :, :]
-                ).float()
-                dec_inp = (
-                    torch.cat(
-                        [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
-                    )
-                    .float()
-                    .to(self.device)
-                )
+                # dec_inp = torch.zeros_like(
+                #     batch_y[:, -self.args.pred_len :, :]
+                # ).float()
+                # dec_inp = (
+                #     torch.cat(
+                #         [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
+                #     )
+                #     .float()
+                #     .to(self.device)
+                # )
                 # encoder - decoder
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
                         if self.args.output_attention:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )[0]
                         else:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )
                 else:
                     if self.args.output_attention:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )[0]
 
                     else:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )
 
                 f_dim = -1 if self.args.features == "MS" else 0
-                outputs = outputs[:, -self.args.pred_len :, f_dim:]
-                batch_y = batch_y[:, -self.args.pred_len :, f_dim:].to(
-                    self.device
-                )
+                # outputs = outputs[:, -self.args.pred_len :, f_dim:]
+                # batch_y = batch_y[:, -self.args.pred_len :, f_dim:].to(
+                #     self.device
+                # )
+                outputs = outputs.float()
                 outputs = outputs.detach().cpu().numpy()
                 batch_y = batch_y.detach().cpu().numpy()
                 if test_data.scale and self.args.inverse:
@@ -361,17 +365,17 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        mae, mse, rmse, mape, mspe = metric(preds, trues)
-        print("mse:{}, mae:{}".format(mse, mae))
+        acc, conf_matrix, prec, rec, F1 = metric(preds, trues)
+        print("acc:{}, prec:{}, recall:{}, F1:{}".format(acc, prec, rec, F1))
         f = open("result_long_term_forecast.txt", "a")
         f.write(setting + "  \n")
-        f.write("mse:{}, mae:{}".format(mse, mae))
+        f.write("acc:{}, prec:{}, recall:{}, F1:{}".format(acc, prec, rec, F1))
         f.write("\n")
         f.write("\n")
         f.close()
 
         np.save(
-            folder_path + "metrics.npy", np.array([mae, mse, rmse, mape, mspe])
+            folder_path + "metrics.npy", np.array([acc, conf_matrix, prec, rec, F])
         )
         np.save(folder_path + "pred.npy", preds)
         np.save(folder_path + "true.npy", trues)
@@ -399,35 +403,35 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                 batch_y_mark = batch_y_mark.float().to(self.device)
 
                 # decoder input
-                dec_inp = torch.zeros_like(
-                    batch_y[:, -self.args.pred_len :, :]
-                ).float()
-                dec_inp = (
-                    torch.cat(
-                        [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
-                    )
-                    .float()
-                    .to(self.device)
-                )
+                # dec_inp = torch.zeros_like(
+                #     batch_y[:, -self.args.pred_len :, :]
+                # ).float()
+                # dec_inp = (
+                #     torch.cat(
+                #         [batch_y[:, : self.args.label_len, :], dec_inp], dim=1
+                #     )
+                #     .float()
+                #     .to(self.device)
+                # )
                 # encoder - decoder
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
                         if self.args.output_attention:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )[0]
                         else:
                             outputs = self.model(
-                                batch_x, batch_x_mark, dec_inp, batch_y_mark
+                                batch_x, batch_x_mark, batch_y, batch_y_mark
                             )
                 else:
                     if self.args.output_attention:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )[0]
                     else:
                         outputs = self.model(
-                            batch_x, batch_x_mark, dec_inp, batch_y_mark
+                            batch_x, batch_x_mark, batch_y, batch_y_mark
                         )
                 outputs = outputs.detach().cpu().numpy()
                 if pred_data.scale and self.args.inverse:

model/iTransformer_classification.py
@@ -50,47 +50,14 @@ class Model(nn.Module):
             ],
             norm_layer=torch.nn.LayerNorm(configs.d_model),
         )
+        self.fc = nn.Linear(7, 1)
         self.classifier = nn.Linear(
             configs.d_model, self.num_classes, bias=True
         )
 
-    # def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
-    #     if self.use_norm:
-    #         # Normalization from Non-stationary Transformer
-    #         means = x_enc.mean(1, keepdim=True).detach()
-    #         x_enc = x_enc - means
-    #         stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-    #         x_enc /= stdev
-
-    #     _, _, N = x_enc.shape # B L N
-    #     # B: batch_size;    E: d_model;
-    #     # L: seq_len;       S: pred_len;
-    #     # N: number of variate (tokens), can also includes covariates
-
-    #     # Embedding
-    #     # B L N -> B N E                (B L N -> B L E in the vanilla Transformer)
-    #     enc_out = self.enc_embedding(x_enc, x_mark_enc) # covariates (e.g timestamp) can be also embedded as tokens
-
-    #     # B N E -> B N E                (B L E -> B L E in the vanilla Transformer)
-    #     # the dimensions of embedded time series has been inverted, and then processed by native attn, layernorm and ffn modules
-    #     enc_out, attns = self.encoder(enc_out, attn_mask=None)
-
-    #     # B N E -> B N S -> B S N
-    #     dec_out = self.projector(enc_out).permute(0, 2, 1)[:, :, :N] # filter the covariates
-
-    #     if self.use_norm:
-    #         # De-Normalization from Non-stationary Transformer
-    #         dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
-    #         dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
-
-    #     return dec_out
-
-    # def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
-    #     dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
-    #     return dec_out[:, -self.pred_len:, :]  # [B, L, D]
-
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         if self.use_norm:
+            # Normalization from Non-stationary Transformer
             means = x_enc.mean(1, keepdim=True).detach()
             x_enc = x_enc - means
             stdev = torch.sqrt(
@@ -98,15 +65,65 @@ class Model(nn.Module):
             )
             x_enc /= stdev
 
-        _, _, N = x_enc.shape
+        _, _, N = x_enc.shape  # B L N
+        # B: batch_size;    E: d_model;
+        # L: seq_len;       S: pred_len;
+        # N: number of variate (tokens), can also includes covariates
 
-        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        # Embedding
+        # B L N -> B N E                (B L N -> B L E in the vanilla Transformer)
+        enc_out = self.enc_embedding(
+            x_enc, x_mark_enc
+        )  # covariates (e.g timestamp) can be also embedded as tokens
+
+        # B N E -> B N E                (B L E -> B L E in the vanilla Transformer)
+        # the dimensions of embedded time series has been inverted, and then processed by native attn, layernorm and ffn modules
         enc_out, attns = self.encoder(enc_out, attn_mask=None)
 
-        # Apply the classifier to the last output of the encoder
-        class_scores = self.classifier(enc_out[:, -1, :])
+        # B N E -> B N S -> B S N
+        dec_out = self.classifier(enc_out).permute(0, 2, 1)[
+            :, :, :N
+        ]  # filter the covariates
 
-        # Apply softmax to convert raw scores into probabilities
-        class_probs = F.softmax(class_scores, dim=-1)
+        if self.use_norm:
+            # De-Normalization from Non-stationary Transformer
+            dec_out = dec_out * (
+                stdev[:, 0, :].unsqueeze(1).repeat(1, self.num_classes, 1)
+            )
+            dec_out = dec_out + (
+                means[:, 0, :].unsqueeze(1).repeat(1, self.num_classes, 1)
+            )
 
-        return class_probs
+        return dec_out
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+        dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+        # print("dec out shape", dec_out.shape)
+        # print("dec out", dec_out[:, -self.num_classes:, :].shape, dec_out[:, -self.num_classes:, :])
+        dec_out = self.fc(dec_out)
+        dec_out = dec_out.squeeze(-1)
+        # print("dec out", dec_out.shape, dec_out)
+        # return dec_out[:, -self.num_classes:, :]  # [B, L, D]
+        return dec_out
+
+    # def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+    #     if self.use_norm:
+    #         means = x_enc.mean(1, keepdim=True).detach()
+    #         x_enc = x_enc - means
+    #         stdev = torch.sqrt(
+    #             torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5
+    #         )
+    #         x_enc /= stdev
+
+    #     _, _, N = x_enc.shape
+
+    #     enc_out = self.enc_embedding(x_enc, x_mark_enc)
+    #     enc_out, attns = self.encoder(enc_out, attn_mask=None)
+
+    #     # Apply the classifier to the last output of the encoder
+    #     class_scores = self.classifier(enc_out[:, -1, :])
+
+    #     # Apply softmax to convert raw scores into probabilities
+    #     class_probs = F.softmax(class_scores, dim=-1)
+
+    #     return class_probs

utils/metrics.py
@@ -1,13 +1,25 @@
 import numpy as np
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+    precision_score,
+    recall_score,
+    f1_score,
+)
 
 
 def RSE(pred, true):
-    return np.sqrt(np.sum((true - pred) ** 2)) / np.sqrt(np.sum((true - true.mean()) ** 2))
+    return np.sqrt(np.sum((true - pred) ** 2)) / np.sqrt(
+        np.sum((true - true.mean()) ** 2)
+    )
 
 
 def CORR(pred, true):
     u = ((true - true.mean(0)) * (pred - pred.mean(0))).sum(0)
-    d = np.sqrt(((true - true.mean(0)) ** 2 * (pred - pred.mean(0)) ** 2).sum(0))
+    d = np.sqrt(
+        ((true - true.mean(0)) ** 2 * (pred - pred.mean(0)) ** 2).sum(0)
+    )
     return (u / d).mean(-1)
 
 
@@ -38,9 +50,37 @@ def metric(pred, true):
     mape = MAPE(pred, true)
     mspe = MSPE(pred, true)
 
-    return mae, mse, rmse, mape, mspe
+    acc = accuracy(pred, true)
+    conf_matrix = confusion_matrix_score(pred, true)
+    prec = precision(pred, true)
+    rec = recall(pred, true)
+    F1 = f1(pred, true)
+
+    return acc, conf_matrix, prec, rec, F1
 
 
 # Metrics for time series classification
 def accuracy(pred, true):
-    return np.mean((pred == true).astype(int))
+    return accuracy_score(true, pred)
+
+
+def confusion_matrix_score(pred, true):
+    return confusion_matrix(true, pred)
+
+
+def precision(pred, true):
+    return precision_score(true, pred)
+
+
+def recall(pred, true):
+    return recall_score(true, pred)
+
+
+def f1(pred, true):
+    return f1_score(true, pred)
+
+
+# print(confusion_matrix(y_test, predictions))
+
+# # plot_confusion_matrix function is used to visualize the confusion matrix
+# plot_confusion_matrix(classifier, X_test, y_test)